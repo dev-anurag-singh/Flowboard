3e48a4bfd06afd3dd37ac7ff8924dd2a04d53ef0
Anurag Kumar <dev.anuragkr@gmail.com>
2026-03-31 23:01:31 +0530

    feat: add logo component

---
+++ components/Logo.tsx
@@ -0,0 +1,99 @@
+export function LogoFull() {
+  return (
+    <svg
+      width="165"
+      height="26"
+      viewBox="0 0 165 26"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="Group 16">
+        <text
+          x="39.5"
+          y="17.5"
+          fill="#000112"
+          className="fill-[#000112] text-[20px] font-bold tracking-tight dark:fill-foreground"
+          style={{ fontFamily: "var(--font-sans, sans-serif)" }}
+        >
+          Flowboard
+        </text>
+        <g id="Group 15">
+          <rect
+            id="Rectangle"
+            y="0.223999"
+            width="6"
+            height="25"
+            rx="2"
+            fill="#635FC7"
+          />
+          <rect
+            id="Rectangle Copy"
+            opacity="0.75"
+            x="9"
+            y="0.223999"
+            width="6"
+            height="25"
+            rx="2"
+            fill="#635FC7"
+          />
+          <rect
+            id="Rectangle Copy 2"
+            opacity="0.5"
+            x="18"
+            y="0.223999"
+            width="6"
+            height="25"
+            rx="2"
+            fill="#635FC7"
+          />
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export function LogoSmall() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="25"
+      viewBox="0 0 24 25"
+      fill="none"
+    >
+      <rect width="6" height="25" rx="2" fill="#635FC7" />
+      <rect opacity="0.75" x="9" width="6" height="25" rx="2" fill="#635FC7" />
+      <rect opacity="0.5" x="18" width="6" height="25" rx="2" fill="#635FC7" />
+    </svg>
+  );
+}
+
+export function LogoLarge() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="100"
+      height="100"
+      viewBox="0 0 100 100"
+      fill="none"
+    >
+      <rect width="25" height="100" rx="2" fill="#635FC7" />
+      <rect
+        opacity="0.75"
+        x="37.5"
+        width="25"
+        height="100"
+        rx="2"
+        fill="#635FC7"
+      />
+      <rect
+        opacity="0.5"
+        x="75"
+        width="25"
+        height="100"
+        rx="2"
+        fill="#635FC7"
+      />
+    </svg>
+  );
+}
