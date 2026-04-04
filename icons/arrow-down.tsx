f1ac31db22178ab633eba96aa551976092d715a0
Anurag Kumar <dev.anuragkr@gmail.com>
2026-04-05 00:52:50 +0530

    refactor: convert svg icons to tsx components, move illustrations to public

---
+++ icons/arrow-down.tsx
@@ -0,0 +1,14 @@
+export function IconArrowDown(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="10"
+      height="7"
+      viewBox="0 0 10 7"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M1 1L5 5L9 1" stroke="currentColor" strokeWidth="2" />
+    </svg>
+  );
+}
